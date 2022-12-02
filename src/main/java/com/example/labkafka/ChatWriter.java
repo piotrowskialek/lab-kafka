8252c7c2b28d6d917909a00524590e4a3046e861
Michal Starosta <michal.starosta@allegro.pl>
2022-12-02 17:53:30 +0100

    dont send partition key

--- src/main/java/com/example/labkafka/ChatWriter.java
+++ src/main/java/com/example/labkafka/ChatWriter.java
@@ -27,7 +27,7 @@ public class ChatWriter {
 
         String partitionKey = labGroupProperties.getName();
         ListenableFuture<SendResult<String, Message>> future =
-                kafkaTemplate.send(CHAT_TOPIC_NAME, partitionKey, message);
+                kafkaTemplate.send(CHAT_TOPIC_NAME, message);
 
         future.addCallback(new ListenableFutureCallback<>() {
 
